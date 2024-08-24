6da9d4986b6dc82e88630c36cae3f8d7191426b7
Jasleen Dhillon <jasleen@Jasleens-Air.lan>
2024-08-24 01:47:25 -0400

    added q1.py

---
+++ q1.py
@@ -0,0 +1,6 @@
+'''
+Question: A drawer contains red socks and black socks. When socks are drawn at random,
+the probability that both are red is 1/2. 
+a) How small can the number of socks in the drawer be?
+b) How small if the number of black socks is even?
+'''
