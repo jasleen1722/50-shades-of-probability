050c60525f1be9c24b3fbff5dfc795a662640617
Jasleen Dhillon <jasleen@Jasleens-Air.lan>
2024-08-24 02:29:06 -0400

    q1 part a python

--- q1.py
+++ q1.py
@@ -4,3 +4,20 @@ the probability that both are red is 1/2.
 a) How small can the number of socks in the drawer be?
 b) How small if the number of black socks is even?
 '''
+
+def sock_drawer():
+    n = 3
+    r = 2
+    prob = 0
+    while True:
+        for r in range(2, r + 1):
+            prob = (r/n) * ((r-1)/(n-1))
+            if prob == 0.5:
+                break
+        if prob == 0.5:
+            break
+        r += 1
+        n += 1
+    return r, n
+
+sock_drawer()
